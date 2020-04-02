0b236de2e5c44831977542f05e933b8d6a36681d
Geoffroy Piroux <geoffroy.piroux@b12-consulting.com>
2020-04-02 22:44:39 +0200

    Do not commit firebase secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
+import { firebaseConfig } from './firebase'
+
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBasGxiaEvtSx9k_0QJucJKiVpr1jF2jYg",
-    authDomain: "itranslate-6f949.firebaseapp.com",
-    databaseURL: "https://itranslate-6f949.firebaseio.com",
-    projectId: "itranslate-6f949",
-    storageBucket: "itranslate-6f949.appspot.com",
-    messagingSenderId: "611475994038",
-    appId: "1:611475994038:web:50d75871598575656fbd17",
-    measurementId: "G-QQWJSJKF5R"
-  }
+  firebaseConfig: firebaseConfig
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { firebaseConfig } from './firebase'
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBasGxiaEvtSx9k_0QJucJKiVpr1jF2jYg",
-    authDomain: "itranslate-6f949.firebaseapp.com",
-    databaseURL: "https://itranslate-6f949.firebaseio.com",
-    projectId: "itranslate-6f949",
-    storageBucket: "itranslate-6f949.appspot.com",
-    messagingSenderId: "611475994038",
-    appId: "1:611475994038:web:50d75871598575656fbd17",
-    measurementId: "G-QQWJSJKF5R"
-  }
+  firebaseConfig: firebaseConfig
 };
 
 /*
