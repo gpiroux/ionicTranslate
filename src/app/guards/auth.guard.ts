257f6baa0e9881622df326fc35cfa90674fdea2b
Geoffroy Piroux <geoffroy.piroux@b12-consulting.com>
2020-05-15 11:23:09 +0200

    enable persistence across multi tab

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -16,7 +16,6 @@ export class AuthGuard implements CanActivate {
           if (user) return true;
           this.router.navigateByUrl('/login');
         })
-      )
-
+      );
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -33,13 +33,12 @@ export class AuthService {
     this.firestore.firestore.settings({
       cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED
     });
-    this.firestore.firestore.enablePersistence()
+    this.firestore.firestore.enablePersistence({synchronizeTabs: true})
       .then(() => console.log('Enable persistence OK'))
       .catch(err => {
         this.notifications.error('Enable persistence KO: ' + err.message || err)
         console.error('enablePersistence KO', err)
       });
-    
   }
 
   async migrateData() {
